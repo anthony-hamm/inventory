bd910569087c7ad7082041716652f35c22c1d206
anthony-hamm <anthonyhamm31@hotmail.com>
2016-12-09 22:26:23 -0800

    heroku issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208153856) do
+ActiveRecord::Schema.define(version: 20161210061954) do
 
   create_table "entries", force: :cascade do |t|
     t.integer  "store_id"
